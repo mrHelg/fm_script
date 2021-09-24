e273f5f2849f4a09bdff2989beaab921671a22bd
Elena <freshjs.ftm@gmail.com>
2021-09-24 11:03:53 +0300

    Create Variable

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
+//variable
+
 /*
-
-alert(20);
-alert("Hello world!");
+имя переменной
+начинается только с буквы или _ или $
+может быть латиница, без пробелов, цифры в середине или конце, регистрозависимое
+camelCase
+отвечает на вопрос что в ней
 */
+//создание переменной
+let num1;
+//инициализация
+num1 = 33;
+//логирование
+console.log('num1 = ', num1);
 
-//console
+let num2 = 28;
 
-console.log(10);
-console.log(undefined);
-console.log('test');
-console.log('1');
-console.log(null);
-console.log(true);
-console.log(false);
+num1 = 44;
+console.log('num1 = ',num1);
+
+const num3 = 77;
+//num3 = 55;
+
+{
+  let num3='qwerty';
+  console.log('num2 = ', num2);
+  console.log('num3 = ', num3);
+}
 
 
 
