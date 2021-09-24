2c4b67f2dacdd4522bb58387f8d4f83617c42543
Elena <freshjs.ftm@gmail.com>
2021-09-24 11:42:36 +0300

    Example typeof

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
-//variable
+let num = 10;
+console.log(num, typeof num);
 
-/*
-имя переменной
-начинается только с буквы или _ или $
-может быть латиница, без пробелов, цифры в середине или конце, регистрозависимое
-camelCase
-отвечает на вопрос что в ней
-*/
-//создание переменной
-let num1;
-//инициализация
-num1 = 33;
-//логирование
-console.log('num1 = ', num1);
+let str = 'ten';
+console.log(str, typeof str);
 
-let num2 = 28;
+let varNull = null;
+console.log(varNull, typeof varNull);
 
-num1 = 44;
-console.log('num1 = ',num1);
-
-const num3 = 77;
-//num3 = 55;
-
-{
-  let num3='qwerty';
-  console.log('num2 = ', num2);
-  console.log('num3 = ', num3);
-}
-
-
+let varUndefined = undefined;
+console.log(varUndefined, typeof varUndefined);
 
 
 
