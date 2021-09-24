c46677038acd7463b6d0063324107f8a15e5cb20
Elena <freshjs.ftm@gmail.com>
2021-09-24 10:40:00 +0300

    Example hardcode

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+
+alert(20);
+alert("Hello world!");
+*/
+
+//console
+
+console.log(10);
+console.log(undefined);
+console.log('test');
+console.log('1');
+console.log(null);
+console.log(true);
+console.log(false);
+
+
+
+
+
