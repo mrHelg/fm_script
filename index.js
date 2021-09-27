653349b17144a84ddb48cdce8ea6e3cfd8a520f6
Elena <freshjs.ftm@gmail.com>
2021-09-27 09:51:25 +0300

    Add Remainder operator.

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-/*
-false
-0 +0 -0
-'' //пустая строка
-NaN
-null
-undefined
-*/
-
-let condition = '12'+0;
-
-console.log(condition, typeof condition);
-
-if(condition){
-  console.log(condition);
-}
-
+//window.alert(77);
+let num = 81;
+console.log(num%3);
