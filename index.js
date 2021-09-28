03f0d14c063ac0275db281c7712000d0d6832d48
Elena <freshjs.ftm@gmail.com>
2021-09-28 09:37:15 +0300

    Add prompt

--- index.js
+++ index.js
@@ -1,39 +1,9 @@
-let number = 77;
+//alert('I\'m a teacher');
 
-//number = number + 10;
-
-number += 10;
-
-number %= 5;
-
-number **=3;
-
-number /= number;
-
-number *=4;
-
-number = (number-2) * 4;
-
-console.log(number);
-
-
-
-
-let nameUser = 'Elon';
-nameUser = (nameUser +  ' Musk') *2;
-//nameUser += ' Musk'*2 ;
-console.log(number, nameUser);
-
-
-
-//let a=2,b,c,res; bad practice
-
-
-let a = 2;
-let b;
-let c;
-let res;
-
+let userInput = prompt('Enter number: ');
+console.log(userInput);
+console.log(isNaN(Number(userInput)));
+console.log(typeof Number(userInput));
 
 
 
