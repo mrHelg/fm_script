146298bf58d3875fc1e442caf06d8515bef83b3c
Elena <freshjs.ftm@gmail.com>
2021-09-28 11:20:33 +0300

    Add fuction & jsdocs

--- index.js
+++ index.js
@@ -16,25 +16,42 @@
 // console.log(mySecondFunction);
 // console.log(myFirstFunction);
 
+// let userInput1 = prompt('enter first number');
+// let userInput2 = prompt('enter second number');
 
-const getSumTwoNums = function (){
-  let userInput1 = prompt('enter first number');
-  let userInput2 = prompt('enter second number');
+// let checkEmpty = userInput1==='' || userInput1===null || userInput2==='' || userInput2===null;
+// sum;
 
-  let checkEmpty = userInput1==='' || userInput1===null || userInput2==='' || userInput2===null;
-
-  let sum = Number(userInput1) + Number(userInput2);
-
-  if(checkEmpty || isNaN(sum)){
-    console.log('error');
-  }else{
-    console.log(userInput1,'+',userInput2,'=',sum);
+/**
+ * 
+ * @param {string} value 
+ * @returns {boolean}
+ */
+const isNotNumber = function(value){
+  return value==='' || value===null || isNaN(Number(value));
+}
+/**
+ * 
+ * @param {any} num1 
+ * @param {any} num2 
+ * @returns { boolean | number }
+ */
+const getSumTwoNums = function (num1, num2){
+  if(isNotNumber(num1) || isNotNumber(num2)){
+    return false;
   }
+  return Number(num1)+Number(num2);
 }
 
-getSumTwoNums();
+let userInput1 = prompt('enter first number');
+let userInput2 = prompt('enter second number');
 
-
+let result = getSumTwoNums(userInput1, userInput2);
+if(result===false){
+  console.log('error');
+}else{
+  console.log(result);
+}
 
 
 
