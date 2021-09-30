5846c61b8c79fa761a83c62b3552e4c7415ee46f
Elena <freshjs.ftm@gmail.com>
2021-09-30 10:25:33 +0300

    Add if..else if..else

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
-// const isAdult = function(age){
-//   return age>=18 ? 'adult' : 'not adult';
+const data = +prompt('Enter'); 
+
+//пользователь должен ввести положительноe больше ноля
+
+// if(isNaN(data)){
+//   console.log('invalid data');
+// }else{
+//   if(data>0){
+//     if(data%5===0){
+//       console.log('кратно 5');
+//     }else{
+//       if(data%3===0){
+//         console.log('кратно 3');
+//       }else{
+//         if(data%2===0){
+//           console.log('кратно 2');
+//         }else{
+//           console.log('simple number');
+//         }
+//       }
+//     }
+//   }else{
+//     console.log('invalid data');
+//   }
 // }
 
-// console.log(isAdult(150))
 
-
-// const logIsAdult = function(age){
-//   const adult = age>=18 ? 'adult' : 'not adult';
-//   console.log(adult)
-// }
-
-// logIsAdult(15);
-
-
-/*
-Функция запрашивает ввод у пользователя и говорит спасибо, если что-то ввели, или пишет сообщение о том, что ничего не введено.
-
-1) придумать имя для функции
-2) принимает ф-ция аргументы?
-3) запросить данные
-4) проверить данные на существование и вывести сообщение
-
-*/
-
-// const data = prompt('Enter');  //string  ||  ''  null
-
-// const logDataInfo = function(info){
-//   console.log(info);
-//   // if(data){
-//   //   console.log('спасибо')
-//   // }else{ //false '' null undefined NaN  number:0 -0  
-//   //   console.log('вы ничего не ввели')
-//   // }
-//   console.log(info ? 'спасибо, вы ввели: '+ info :'вы ничего не ввели');
-// }
-
-// logDataInfo(data);
+if(isNaN(data)){
+  console.log('invalid data, must be number');//not a number
+}else if(data>0){//positive number
+  if(data%5===0){
+    console.log('кратно 5');
+  }else if(data%3===0){
+    console.log('кратно 3');
+  }else if(data%2===0){
+    console.log('кратно 2');
+  }else{
+    console.log('simple number');
+  }
+}else{//negative number
+    console.log('invalid data, must be positive');
+}
 
 
 
-
-
-const summa = 123574;
-const sale = summa>10000 ? 20 : 
-              summa>5000 ? 10 : 5;
