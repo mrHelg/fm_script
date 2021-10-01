c60d63b7bf8ce24ba80f90d55e3fa16c71fa5cbf
Elena <freshjs.ftm@gmail.com>
2021-10-01 09:59:18 +0300

    Add operator while

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-const sum =  function(a,b){
-  return a+b;
+let count = 0;
+//debugger;
+while(true){
+  count++;
+  if(count%2===1){
+    continue;
+  }
+  if(count > 10){
+    break;
+  }
+  console.log(count);
 }
-const sub = function(a,b){
-  return a-b;
-}
-const mult = function(a,b){
-  return a*b;
-}
-const div = function(a,b){
-  return a/b;
-}
-const rem = function(a,b){
-  return a%b;
-}
-
-const hightOrderFunction = function(num1, num2, func){
-  return func(num1, num2);
-}
-
-
-
-const res = hightOrderFunction( -2, 6, rem);
-console.log(res);
+console.log('End loop');
