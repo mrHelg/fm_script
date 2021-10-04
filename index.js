27ea3733b12795e9742c134840513981ed684021
Elena <freshjs.ftm@gmail.com>
2021-10-04 12:37:07 +0300

    Add Auto

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
-  
-function Cat(name='test', isMale=true, color='grey', breed='pers', age=0){
-  this.name = name;
-  this.isMale = isMale;
-  this.color = color;
-  this.breed = breed;
-  this.age = age;
-  this.say = function(){
-    return 'meow';
-  };
-}
+'use strict';
 
-const catTest = new Cat();
-const catMuha = new Cat('Muha',false,'grey','yard',2);
-const kitty = new Cat('Kitty',false,'white','yard',1);
-const catPushok = new Cat('Pushok',true,'black','pers',4);
-console.log(catPushok.name+' says '+catPushok.say());
+function Auto(name, maxSpeed){
+  this.name = name;
+  this.maxSpeed = maxSpeed;
+  this.speed = 0;
+
+  this.accelerate = function(value){
+    if(value<0){
+      return false;
+    }
+    this.speed += value;
+    if(this.speed>this.maxSpeed){
+      return (this.speed = this.maxSpeed);
+    } 
+    return this.speed;
+  };
+  this.deaccelerate = function(value){
+    if(value<0 || this.speed<value){
+      return false;
+    }
+    this.speed -= value;
+    if(this.speed<0){
+      return (this.speed=0);
+    } 
+    return this.speed;
+  };
+  this.stop = function(){
+    return (this.speed=0);
+  };
+};
+
+const car = new Auto('bmw', 250);
+
+
+
 
 
 
