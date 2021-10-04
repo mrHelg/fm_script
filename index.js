e421f45ed0e5fc03472e94d639e6b7b8eb9aa841
Elena <freshjs.ftm@gmail.com>
2021-10-04 09:21:10 +0300

    Example create object

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-const calcFactorial = function(number=2){
-  if(number<0){
-    return null;
-  }
-  if(number===0 || number===1){
-    return 1;
-  }
-  if(number>MAX_FACTORIAL){
-    return false;
-  }
-  let result = 1n;
-  for(let i=1; i<=number; i++){
-    result *= BigInt(i);
-  }
-  return result;
-}
+const object = {
+  display:'flex',
+  margin:15,
+  justifyContent: 'center',
+  visibility: true,
+  resize:function(){
+    return 'resize';
+  },
+};
 
-console.log(calcFactorial(77));//6
 
+const ob1 = {};
+
+const ob2 = Object();
+const ob3 = new Object();
