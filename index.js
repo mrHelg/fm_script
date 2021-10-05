ee4a38f73e5d5d224ff523e5ce469c15002a2b59
Elena <freshjs.ftm@gmail.com>
2021-10-05 12:21:33 +0300

    Add example multiply table

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
+/*
+const table = {
+  '12 * 2 = ':24,
+}
+*/
 
-const obj = {
-  name:'pen',
-  'is writen': true,
-  11:78,
-};
+const obj ={};
+obj.prop1 = 12;
+obj['prop 2'] = 22;
 
-const nameProperty = 'is writen';
-console.log(obj[11]);
-console.log(obj['11'], obj['11']);
-console.log(obj['is writen']);
-console.log(obj['name']);
-console.log(obj[nameProperty]);
+const createMultiplicationTable = function(min=1, max=10){
+  const table = {};
+  for(let i=min; i<=max; i++){
+    for(let j=1; j<=max; j++){
+      //Доступ к свойствам с возможностью вычисления
+      table[`${i} * ${j} = `] = i*j;
+    }
+  }
+  return table;
+}
+console.log(createMultiplicationTable())
 
+const f = function(str, obj){
+  return obj[str];
+}
 
-
+const str = '2 * 3 = ';
+const table = createMultiplicationTable();
+console.log(f(str, table)); //6
