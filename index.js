ce52564d2bd6f3f546b81e878cbcbd030c30fa80
Elena <freshjs.ftm@gmail.com>
2021-10-06 09:23:51 +0300

    Example copy obj by link

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-/*
-const table = {
-  '12 * 2 = ':24,
-}
-*/
 
-const obj ={};
-obj.prop1 = 12;
-obj['prop 2'] = 22;
+const t1 = 1;
+const t2 = 1;
 
-const createMultiplicationTable = function(min=1, max=10){
-  const table = {};
-  for(let i=min; i<=max; i++){
-    for(let j=1; j<=max; j++){
-      //Доступ к свойствам с возможностью вычисления
-      table[`${i} * ${j} = `] = i*j;
-    }
-  }
-  return table;
-}
-console.log(createMultiplicationTable())
+const obj1 = {};
+const obj2 = {};
 
-const f = function(str, obj){
-  return obj[str];
-}
+const link = obj1;//копирование по ссылке
 
-const str = '2 * 3 = ';
-const table = createMultiplicationTable();
-console.log(f(str, table)); //6
+console.log(obj1===link);//true
+console.log(obj2===link);//false
+
+link.prop2 = 44;
