1946d3ca7decd5ad78e577ab7fe6e375ca1e0174
Elena <freshjs.ftm@gmail.com>
2021-10-06 11:50:28 +0300

    Refactor MyArray

--- index.js
+++ index.js
@@ -1,27 +1,54 @@
 /* logic */
 function MyArrayProto(){
-  this.push = function(item){
-      this[this.length] = item;
-      //this.length++;
-      return ++this.length;
+  this.push = function(){
+    for (let i = 0; i < arguments.length; i++) {
+      this[this.length++] = arguments[i];
+    }
+    return this.length;
+  }
+  this.pop = function(){
+    if(this.length===0){
+      return;
+    }
+    const item = this[--this.length];
+    delete this[this.length];
+    return item;
   }
 }
 /* data */
 function MyArray(){
   this.length = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    this.push(arguments[i]);
+  }
 }
 /* Prototype */
 MyArray.prototype = new MyArrayProto();
+MyArray.prototype.newMethod = function(){};
 
-const myArray = new MyArray();
-const myArray2 = new MyArray();
-//console.log(myArray);
-console.log(myArray.push===myArray2.push);//true
-
+const myArray = new MyArray(1,1,1,15,5);
+myArray.push(2,2,2);
 
 
-const arr = [];
-arr.push(1);//откуда взялся push????
-//console.log(arr);
-const arr2 = [];
-console.log(arr.push===arr2.push);//true
+
+
+
+
+
+
+
+/*
+function sum(){
+  console.log(arguments)
+  let result = 0;
+  for (let index = 0; index < arguments.length; index++) {
+    result += arguments[index];
+  }
+  return result;
+}
+
+console.dir(sum)
+
+sum(1,2,3,74,8,4,65,8)
+
+*/
