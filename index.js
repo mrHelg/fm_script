24375a73df1f7aba75deef3d550570a757889bb3
Elena <freshjs.ftm@gmail.com>
2021-10-06 10:41:17 +0300

    Add MyArray, method push

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 
-// const obj1 = {
-//   name:'obj1',
-// };
-// const obj2 = new Object({name:'obj2',});
+function MyArray(){
+  this.length = 0;
 
-/*
-const ph1 = '271-45-45';
-const ph2 = '271-55-45';
-const ph3 = '271-45-77';
-
-const phBook = {
-  1 : '271-45-45',
-  2 : '271-55-45',
-  3 : '271-45-77',
-  2 : '471-33-33',
+  this.push = function(item){
+    this[this.length] = item;
+    this.length++;
+    return this.length;
+  }
 }
 
-*/
+const myArray = new MyArray();
+const myArray2 = new MyArray();
+//console.log(myArray);
+console.log(myArray.push===myArray2.push);//false
 
 
-const phArray = new Array('271-45-45', '271-55-45','271-45-77');
-const phArray2 = ['271-45-45', '271-55-45','271-45-77'];
 
+const arr = [];
+arr.push(1);//откуда взялся push????
+//console.log(arr);
+const arr2 = [];
+console.log(arr.push===arr2.push);//true
