dd2d17acf4ad5c12d602c6366b75311c2f1850c3
Elena <freshjs.ftm@gmail.com>
2021-10-07 09:57:29 +0300

    Change method prototype

--- js/index.js
+++ js/index.js
@@ -1,2 +1,25 @@
+function toString(){
+    return `${this.firstName} ${this.lastName}`;
+}
 
 
+const student1 = {
+  id:1,
+  firstName:'Elon',
+  lastName:'Musk',
+  age:50,
+  isMale:true,
+  //toString:toString,
+  toString,
+};
+console.log(student1.toString())
+
+const student2 = {
+  id:2,
+  firstName:'Tim',
+  lastName:'Li',
+  age:65,
+  isMale:true,
+  toString,
+};
+console.log(student2.toString())
