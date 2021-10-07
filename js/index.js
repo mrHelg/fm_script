3ca77aadb2b6618a1db5d570a0c4a30cb1085890
Elena <freshjs.ftm@gmail.com>
2021-10-07 10:17:55 +0300

    Add __proto__

--- js/index.js
+++ js/index.js
@@ -2,7 +2,17 @@ function toString(){
     return `${this.firstName} ${this.lastName}`;
 }
 
-
+const animal = {
+  go:function(){
+    return `${this.firstName} go!`;
+  }
+};
+const man = {
+  eat:function(){
+    return `${this.firstName} eating`;
+  }
+};
+man.__proto__ = animal;
 const student1 = {
   id:1,
   firstName:'Elon',
@@ -12,7 +22,16 @@ const student1 = {
   //toString:toString,
   toString,
 };
-console.log(student1.toString())
+student1.__proto__ = man;
+
+console.log(student1);
+
+
+
+
+
+
+
 
 const student2 = {
   id:2,
@@ -22,4 +41,5 @@ const student2 = {
   isMale:true,
   toString,
 };
-console.log(student2.toString())
+student2.__proto__ = man;
+
