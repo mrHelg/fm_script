26d6b0547c3de3c33aa641895430f7df15058461
Elena <freshjs.ftm@gmail.com>
2021-10-08 11:18:28 +0300

    Create array of users

--- js/index.js
+++ js/index.js
@@ -1,11 +1,34 @@
-
-const users = [{id:1,},{id:2,},{id:3,}];
-
-function addSubscribe(user){
-  user.isSubscribed = true;
+function User(firstName, lastName, age, isMale, email, isSubscribed = false){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.isMale = isMale;
+  this.email = email;
+  this.isSubscribed = isSubscribed;
 }
 
-users.forEach(addSubscribe);
+function UserPrototype(){
+  this.fullName = function(){
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
 
+User.prototype = new UserPrototype();
 
+function createRandomUsers(amount = 1){
+  const db = [];
+  for(let i=0; i<amount; i++){
+    const user = new User(
+      `Name${i}`,
+      `Lname${i}`,
+      Math.ceil(Math.random()*60+20),//20-80
+      Math.random()>0.5, //true||false
+      `email${i}@gmail.com`
+    );
+    db.push(user);
+  }
+  return db;
+}
 
+const users = createRandomUsers(50);
+console.table(users);
