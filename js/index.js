72f9070c8d95a9bddb9d7cf2b4838e1ee4c0e284
Elena <freshjs.ftm@gmail.com>
2021-10-08 09:25:17 +0300

    Add HOF

--- js/index.js
+++ js/index.js
@@ -1,29 +1,16 @@
-function Ladder(){
-  this.value = 0;
+/**
+ * 
+ * @param {function} howSay 
+ * @param {string} whatSay 
+ */
+function saySomething(howSay, whatSay){
+  howSay(whatSay);
 }
 
-function LadderPrototype(){
-  this.up = function(){
-    this.value++;
-    return this;
-  }
-  this.down = function(){
-    this.value--;
-    return this;
-  }
-  this.showStep = function(){
-    return this.value;
-  }
-}
 
-Ladder.prototype = new LadderPrototype();
+saySomething(console.log, 'qwerty');
+//console.log('qwerty');
 
-const ladder1 = new Ladder();
-
-
-const step = ladder1.up().down().up().up().down().showStep();
-
-console.log(step);
-
-
-
+saySomething(alert, 'alert');
+saySomething(prompt, 'prompt');
+saySomething(confirm, 'confirm');
