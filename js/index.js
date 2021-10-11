64049a6e8294bfc9a048273500b505b005eab78e
Elena <freshjs.ftm@gmail.com>
2021-10-11 10:14:52 +0300

    Add bind

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 'use strict';
 
+function logItem(header, index){
+  console.log(this.title);
+  console.log(`${index+1}: ${header}`);
+}
+
 const site = {
   title:'Green site',
   headers:['Header1','Test','Header3','New article'],
   showHeaders(){
     //contecst
-    this.headers.forEach(function(header, index){
-      console.log(this.title)
-      console.log(`${index+1}: ${header}`);
-    },this);
+    this.headers.forEach(logItem.bind(this));
   }
 };
 
