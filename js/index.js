001fe45719cbc4e30ee63c7d0019f91278561b67
Elena <freshjs.ftm@gmail.com>
2021-10-11 09:39:22 +0300

    Add arrow function

--- js/index.js
+++ js/index.js
@@ -1,53 +1,24 @@
-function User(firstName, lastName, age, isMale, email, isSubscribed = false){
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.age = age;
-  this.isMale = isMale;
-  this.email = email;
-  this.isSubscribed = isSubscribed;
-}
-function UserPrototype(){
-  this.fullName = function(){
-    return `${this.firstName} ${this.lastName}`;
-  }
-}
-User.prototype = new UserPrototype();
+'use strict';
 
-function createRandomUsers(amount = 1){
-  const db = [];
-  for(let i=0; i<amount; i++){
-    const user = new User(
-      `Name${i}`,
-      `Lname${i}`,
-      Math.ceil(Math.random()*60+20),//20-80
-      Math.random() > 0.5, //true||false
-      `email${i}@gmail.com`
-    );
-    db.push(user);
-  }
-  return db;
+//декларация
+function test1(){
+  console.log(this)
 }
 
-const users = createRandomUsers(100);
-users.forEach(function(user){
-  user.isSubscribed = (Math.random()>0.5);
-});
-console.table(users);
-
-/* получить массив полных имен пользователей, используя map */
-const fullNameUsers = users.map(function(user){
-  return user.fullName();
-});
-//console.table(fullNameUsers);
-
-/* получить массив пользователей, которые старше 65, используя filter*/
-function isOldPeople(user){
-  return user.age>=OLD_AGE;
+//выражение
+const test2 = function(){
+  console.log(this)
 }
-const oldUsers = users.filter(isOldPeople);
-//console.table(oldUsers);
+
+//стрелка
+const test3 = ()=>{
+  console.log(this)
+}
+
+test1();
+test2();
+test3();
 
 
-/*
-получить массив пользователей до 40 лет женского пола у которых есть подписка
-*/
+
+
