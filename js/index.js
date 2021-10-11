ad4c8529da3f5901cb5e3c651e1acb0741f996bf
Elena <freshjs.ftm@gmail.com>
2021-10-11 11:08:30 +0300

    Add sum with ...

--- js/index.js
+++ js/index.js
@@ -1,16 +1,14 @@
 'use strict';
 
-const t1 = function(...qq){
-  console.log(arguments)
-  console.log(qq)
-}
-
-const t2 = (num, ...rest)=>{
-  console.log('num = ',num)
-  console.log('rest = ',rest)
-}
-
-t1(1,2,3,4);
-t2(22,4,8,5);
 
 //sum, которая принимает неограниченное кол-во пареметров
+
+const sum = (...args)=>{
+  let result = null;
+  args.forEach((n)=>{
+    result += n;
+  });
+  return result;
+}
+
+const sum2 = (...args)=> args.reduce((result, n)=>result+n);
