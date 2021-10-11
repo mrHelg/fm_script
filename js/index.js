70a96e4144a1c8a882f66829b56d66de99079e1b
Elena <freshjs.ftm@gmail.com>
2021-10-11 11:33:39 +0300

    Add recursion

--- js/index.js
+++ js/index.js
@@ -1,19 +1,12 @@
 'use strict';
 
-
-//sum, которая принимает неограниченное кол-во пареметров
-
-const sum = (...args)=>{//rest
-  let result = null;
-  args.forEach((n)=>{
-    result += n;
-  });
-  return result;
+function recursion(num){
+  debugger;
+  if(num===0){
+    return;
+  }
+  console.log(num);
+  recursion(num-1);
 }
 
-const sum2 = (...args)=> args.reduce((result, n)=>result+n);
-
-
-const arrNums1 = [4,1,3,6];
-console.log(sum(...arrNums1));//spred
-console.log(sum2(...arrNums1));
+recursion(4);
