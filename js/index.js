036a61a235d35861d84ccbc1a0abb62b887efbd4
Elena <freshjs.ftm@gmail.com>
2021-10-11 11:15:15 +0300

    Add spred operator

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 
 //sum, которая принимает неограниченное кол-во пареметров
 
-const sum = (...args)=>{
+const sum = (...args)=>{//rest
   let result = null;
   args.forEach((n)=>{
     result += n;
@@ -12,3 +12,8 @@ const sum = (...args)=>{
 }
 
 const sum2 = (...args)=> args.reduce((result, n)=>result+n);
+
+
+const arrNums1 = [4,1,3,6];
+console.log(sum(...arrNums1));//spred
+console.log(sum2(...arrNums1));
