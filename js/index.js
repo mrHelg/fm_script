88c6fadaf680bb348fefa3cbbd5c0e085cbe9d07
Elena <freshjs.ftm@gmail.com>
2021-10-11 10:10:17 +0300

    Add thisArg for forEach

--- js/index.js
+++ js/index.js
@@ -5,11 +5,10 @@ const site = {
   headers:['Header1','Test','Header3','New article'],
   showHeaders(){
     //contecst
-    const self = this;
     this.headers.forEach(function(header, index){
-      console.log(self.title)
+      console.log(this.title)
       console.log(`${index+1}: ${header}`);
-    });
+    },this);
   }
 };
 
