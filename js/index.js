021477b728bba96231c4e7c582a5be92bf24df5f
Elena <freshjs.ftm@gmail.com>
2021-10-11 12:02:50 +0300

    Add example recursion

--- js/index.js
+++ js/index.js
@@ -1,12 +1,23 @@
 'use strict';
-
-function recursion(num){
-  debugger;
-  if(num===0){
-    return;
+//2**5 = 2* (2**4) = 2*2*  2**3
+const power = (num, exp)=>{
+  if(exp===1){
+    return num;
   }
-  console.log(num);
-  recursion(num-1);
+  return num * power(num, exp-1);
 }
 
-recursion(4);
+power(2,3);
+
+
+const factorial = (num)=>{
+  if(num===0){
+    return 1;
+  }
+  return  num * factorial(num-1);
+}
+
+factorial(4);
+
+
+
