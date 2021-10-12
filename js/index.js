956ee4839c4117f8ffec748b3bda0e015d343490
Elena <freshjs.ftm@gmail.com>
2021-10-12 11:03:08 +0300

    Add new Number

--- js/index.js
+++ js/index.js
@@ -1,18 +1,13 @@
 'use strict';
 
-const boolean = new Boolean(null);
-console.log(boolean)//object
+const number = new Number(10);
+console.dir(number);
+console.dir(123123.456789.toFixed(2));
 
-if(boolean.valueOf()){
-  console.log('true')
-}else{
-  console.log('false')
-}
+const num = Number(123123.456789.toFixed(2));
+console.log(num);
 
-const str = '';
-if(Boolean(str)){
-  console.log(Boolean(str))
-}else{
-  console.log('false')
-}
 
+let num2 = 12.123456;//объектом-обёрткой через конструктор Number()
+//num2 = num2.toFixed(2);
+console.dir(num2);
