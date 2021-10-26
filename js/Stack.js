c304db4d093f36ba943615cbd2c11b6b8383b8e3
Олег <osumsky@gmail.com>
2021-10-26 09:49:12 +0300

    Rest index.js

--- js/Stack.js
+++ js/Stack.js
@@ -1,5 +1,5 @@
 class Stack {
-  constructor(maxSize, ...args) {
+  constructor(maxSize = 5, ...args) {
     this._maxSize = maxSize;
     this._size = 0;
     for (const argument of args) {
@@ -15,7 +15,29 @@ class Stack {
     return this._size === 0;
   }
 
-  push(argument) {}
-  pick() {}
-  pop() {}
+  push(argument) {
+    if (this.size >= this._maxSize) {
+      throw new RangeError('Stack overflow');
+    }
+    this[`_${this.size}`] = argument;
+    this._size++;
+    return this.size;
+  }
+
+  pick() {
+    return this[`_${this.size - 1}`];
+  }
+
+  pop() {
+    if (this.size <= 0) {
+      return;
+    }
+    const lastItem = this[`_${this.size - 1}`];
+    delete this[`_${this.size - 1}`];
+    this._size--;
+    return lastItem;
+  }
 }
+
+const stack = new Stack(10, 77, 78, 79);
+
