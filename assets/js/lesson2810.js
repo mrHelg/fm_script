77f5c1aa78fc6bc803cb7598f140753657a506d3
Олег <osumsky@gmail.com>
2021-10-29 09:16:27 +0300

    Add data.js

--- assets/js/lesson2810.js
+++ assets/js/lesson2810.js
@@ -1,12 +1,28 @@
-'use strict'
+'use strict';
 
 const root = document.getElementById('root');
 const btns = document.getElementsByTagName('button');
 
-const changeRootBackground = ({target:{dataset:{color}, parentElement: parent}}) => {
-  parent.style.background = color;
-}
+const changeRootBackground = ({
+  target: {
+    dataset: { color },
+    parentElement: parent,
+  },
+}) => {
+  parent.setAttribute('style', `background-color: ${color}`);
+  // parent.style.background = color;
+  console.log(color);
+};
 
 for (const btn of btns) {
   btn.addEventListener('click', changeRootBackground);
 }
+
+// const handler = ({currentTarget}) =>{
+//   console.dir(currentTarget, ' is dipping');
+// }
+
+// document.getElementById('root').addEventListener('click', handler, {once: true, capture: true});
+// document.addEventListener('click', handler, true);
+// document.body.addEventListener('click', handler, true);
+// window.addEventListener('click', handler, true);
