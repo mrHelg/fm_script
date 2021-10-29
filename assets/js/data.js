a39c82ac4f777e186c03c3f83af4369e3541bb56
Олег <osumsky@gmail.com>
2021-10-29 11:51:45 +0300

    Fixed errors

--- assets/js/data.js
+++ assets/js/data.js
@@ -11,34 +11,34 @@ const actors = [
     name: 'Robert Downey',
     birthdate: 'April 4, 1965',
     photo:
-      'https://tr.web.img3.acsta.net/c_310_420/pictures/210/017/21001798_20130426172912097.jpg',
+      'https://tr.web.img3.acsta.net/c_310_420/pictures/210/017/21001798_20130426172912097.jpg2',
   },
   {
     id: 3,
     name: 'Johnny Depp',
     birthdate: 'June 9, 1963',
     photo:
-      'https://i.pinimg.com/originals/88/32/e0/8832e023b0a18e446a390eb9a3566590.jpg',
+      'https://i.pinimg.com/originals/88/32/e0/8832e023b0a18e446a390eb9a3566590.jpg1',
   },
   {
     id: 4,
     name: 'Brad Pitt',
     birthdate: 'December 18, 1963',
     photo:
-      'https://cdn.7days.ru/upload/images/72c/543959d2563fca308622fe515bffd.jpg',
+      'https://cdn.7days.ru/upload/images/72c/543959d2563fca308622fe515bffd.jpg3',
   },
   {
     id: 5,
     name: 'Jared Leto',
     birthdate: 'December 26, 1971',
     photo:
-      'https://bodysize.org/wp-content/uploads/2017/03/Jared-Leto-300x400.jpg',
+      'https://bodysize.org/wp-content/uploads/2017/03/Jared-Leto-300x400.jpg7',
   },
   {
     id: 6,
     name: 'Gerard Butler',
     birthdate: 'November 13, 1969',
     photo:
-      'https://hairstyles.thehairstyler.com/hairstyle_views/front_view_images/4555/original/Gerard-Butler.jpg',
+      'https://hairstyles.thehairstyler.com/hairstyle_views/front_view_images/4555/original/Gerard-Butler.jpg7',
   },
 ];
